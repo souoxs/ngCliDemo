b36412ce26501efe874372ce8b7befd4c09d0833
lilei <lilei@jiedaibao.com>
2018-03-14 16:16:06 +0800

    add event and input and life

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { IndexComponent } from './index/index.component';
+import { EventComponent } from './event/event.component';
+import { InputComponent } from './input/input.component';
+import { LifeComponent } from './life/life.component';
 
 
 @NgModule({
@@ -14,7 +17,10 @@ import { IndexComponent } from './index/index.component';
     AppComponent,
     ContactComponent,
     AboutComponent,
-    IndexComponent
+    IndexComponent,
+    EventComponent,
+    InputComponent,
+    LifeComponent
   ],
   imports: [
     BrowserModule,
